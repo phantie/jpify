bfd0c3edf9bd04045cf54765e77a3272246168d5
phantie <funtom1605@gmail.com>
2021-01-07 18:22:54 +0200

    here we go

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to loju
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/loju/core_test.clj
@@ -0,0 +1,7 @@
+(ns loju.core-test
+  (:require [clojure.test :refer :all]
+            [loju.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
